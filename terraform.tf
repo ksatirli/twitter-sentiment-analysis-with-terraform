dadff6bfac167e15929224a052030ada019dd56a
Kerim Satirli <k.satirli@gmail.com>
2021-10-13 15:24:51 +0200

    sets up Terraform requirements

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-provider-requirements
+  required_providers {
+    # see https://registry.terraform.io/providers/hashicorp/google/3.87.0
+    google = {
+      source  = "hashicorp/google"
+      version = "3.87.0"
+    }
+  }
+
+  # see https://www.terraform.io/docs/language/settings/index.html#specifying-a-required-terraform-version
+  required_version = "1.0.8"
+}
