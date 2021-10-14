ebab89ada82f5b6154a047f8ef31ac0f968071bf
Kerim Satirli <k.satirli@gmail.com>
2021-10-14 12:38:32 +0200

    linting

--- outputs.tf
+++ outputs.tf
@@ -1,35 +1,42 @@
 # see https://www.terraform.io/docs/language/values/outputs.html
-output "console_url_project_settings" {
+output "url_project_settings" {
   description = "URL for Project Settings in Console"
   value       = "${local.console_url_prefix}/iam-admin/settings?${local.console_url_suffix}"
 }
 
 # see https://www.terraform.io/docs/language/values/outputs.html
-output "console_url_service_account_overview" {
+output "url_service_account_overview" {
   description = "URL for Service Account Overview in Console"
   value       = "${local.console_url_prefix}/iam-admin/serviceaccounts?${local.console_url_suffix}"
 }
 
 # see https://www.terraform.io/docs/language/values/outputs.html
-output "console_url_service_account_detail" {
+output "url_service_account_detail" {
   description = "URL for Service Account Detail in Console"
   value       = "${local.console_url_prefix}/iam-admin/serviceaccounts/details/${google_service_account.sentiment_analysis.unique_id}?${local.console_url_suffix}"
 }
 
 # see https://www.terraform.io/docs/language/values/outputs.html
-output "console_url_workflows_overview" {
+output "url_workflows_overview" {
   description = "URL for Workflows Overview in Console"
   value       = "${local.console_url_prefix}/workflows?${local.console_url_suffix}"
 }
 
 # locals values for convenient usage of long Workflows URLs
 locals {
-  workflows_url_prefix = "${local.console_url_prefix}/workflows/workflow/${var.google_project_region}"
-  workflows_url_suffix = "${google_workflows_workflow.sentiment_analysis.name}/executions?${local.console_url_suffix}"
+  workflow_details_url_prefix = "${local.console_url_prefix}/workflows/workflow/${var.google_project_region}"
+  workflow_details_url_suffix = "${google_workflows_workflow.sentiment_analysis.name}/executions?${local.console_url_suffix}"
+  workflow_execution_url_suffix = "${google_workflows_workflow.sentiment_analysis.name}/create_execution?${local.console_url_suffix}"
 }
 
 # see https://www.terraform.io/docs/language/values/outputs.html
-output "console_url_workflow_details" {
+output "url_workflow_details" {
   description = "URL for Workflow Details in Console"
-  value       = "${local.workflows_url_prefix}/${local.workflows_url_suffix}"
+  value       = "${local.workflow_details_url_prefix}/${local.workflow_details_url_suffix}"
+}
+
+# see https://www.terraform.io/docs/language/values/outputs.html
+output "url_workflow_execution" {
+  description = "URL for Workflow Execution in Console"
+  value       = "${local.workflow_details_url_prefix}/${local.workflow_details_url_suffix}"
 }
