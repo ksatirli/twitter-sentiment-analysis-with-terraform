c797b9ccf84c2ba7cfbb9b1b8c19416b213b4adb
Kerim Satirli <k.satirli@gmail.com>
2021-10-14 10:37:29 +0200

    adds Project and SAs and random string for project names

---
+++ main.tf
@@ -0,0 +1,66 @@
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_organization_policy
+#resource "google_organization_policy" "skip_default_network_creation" {
+#  org_id     = var.google_org_id
+#  constraint = "constraints/compute.skipDefaultNetworkCreation"
+#
+#  boolean_policy {
+#    enforced = true
+#  }
+#}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project
+resource "google_project" "sentiment_analysis" {
+  billing_account = var.google_billing_account
+
+  # If your setup requires the usage of `folder_id`, uncomment `folder_id` on the
+  # next line and edit `variables.tf` to uncomment the `google_folder_id` variable.
+  # folder_id = var.google_folder_id
+
+  labels = local.labels
+  name   = var.google_project_name
+
+  # Comment the next line if your setup requires `folder_id` instead of `org_id`
+  org_id = var.google_org_id
+
+  project_id = local.google_project_id
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
+resource "google_service_account" "sentiment_analysis" {
+  account_id   = local.google_project_id
+  display_name = var.google_project_name
+  project      = google_project.sentiment_analysis.project_id
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
+resource "google_project_iam_member" "sentiment_analysis" {
+  # see https://www.terraform.io/docs/language/meta-arguments/for_each.html
+  for_each = {
+    for role in var.iam_roles :
+    role => role
+  }
+
+  member  = "serviceAccount:${google_service_account.sentiment_analysis.email}"
+  project = google_project.sentiment_analysis.project_id
+  role    = each.key
+}
+
+resource "google_project_service" "services" {
+  # see https://www.terraform.io/docs/language/meta-arguments/for_each.html
+  for_each = {
+    for service in var.project_services :
+    service => service
+  }
+
+  disable_dependent_services = false
+  disable_on_destroy         = true
+  project                    = google_project.sentiment_analysis.project_id
+  service                    = each.key
+
+  # see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service#timeouts
+  timeouts {
+    create = "30m"
+    update = "45m"
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -12,3 +12,70 @@ variable "google_project_region" {
   }
 }
 
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project#billing_account
+variable "google_billing_account" {
+  type        = string
+  description = "The alphanumeric ID of the billing account this project belongs to."
+}
+
+# Depending on your setup, you may need to use `google_folder_id` instead of `google_org_id`.
+# If that is the case, uncomment the next lines and update `main.tf` in the appropriate location.
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project#folder_id
+#variable "google_folder_id" {
+#  type        = number
+#  description = "The numeric ID of the folder this project belongs to."
+#}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project#name
+variable "google_org_id" {
+  type        = number
+  description = "The numeric ID of the organization this project belongs to."
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project#name
+variable "google_project_name" {
+  type        = string
+  description = "The display name of the project."
+  default     = "Twitter Sentiment Analysis"
+
+  validation {
+    condition = (
+      length(var.google_project_name) >= 4
+      && length(var.google_project_name) <= 30
+    )
+    error_message = "The Project Name must be between 4 and 30 characters long."
+  }
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project#name
+variable "google_project_id" {
+  type        = string
+  description = "The project ID."
+  default     = "twitter-sentiment"
+
+  validation {
+    condition     = (length(var.google_project_id) <= 25)
+    error_message = "The Project ID must be at most 25 characters long."
+  }
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam#role
+variable "iam_roles" {
+  type        = list(string)
+  description = "The list of role(s) that should be applied."
+
+  default = [
+    "roles/serviceusage.serviceUsageConsumer"
+  ]
+}
+
+# see https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_service#service
+variable "project_services" {
+  type        = list(string)
+  description = "List of services to enable."
+
+  default = [
+    "workflows.googleapis.com"
+  ]
+}
+
